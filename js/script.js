6ea7551b188964f6021da2811df07391c6b64b36
amadirfan <amadirfan443@gmail.com>
2024-02-25 15:38:33 +0500

    setting lay out

--- js/script.js
+++ js/script.js
@@ -1,6 +1,41 @@
+// @ts-nocheck
+// document.addEventListener("DOMContentLoaded", () => {
+// });
 
+let slideIndex = 0;
+let nav_menu = document.getElementsByClassName("nav-menu")[0];
+let btnMenuShow = document.getElementById("responsive-btn");
+const slides = document.getElementsByClassName("slide");
 
+btnMenuShow?.addEventListener("click", () => {
+  nav_menu.classList.toggle("show-nav-menu");
+});
 
+nav_menu.addEventListener("click", () => {
+  nav_menu.classList.toggle("show-nav-menu");
+});
 
+function showSlide(n) {
+  for (let i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slides[n].style.display = "block";
+}
 
-document.addEventListener("DOMContentLoaded", () => { });
+function nextSlide() {
+  slideIndex++;
+  if (slideIndex >= slides.length) {
+    slideIndex = 0;
+  }
+  showSlide(slideIndex);
+}
+
+function prevSlide() {
+  slideIndex--;
+  if (slideIndex < 0) {
+    slideIndex = slides.length - 1;
+  }
+  showSlide(slideIndex);
+}
+
+showSlide(slideIndex);
