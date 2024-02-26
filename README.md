ea7f3eed5231d459ec7d55dbd2a363d8aa5f8c8b
amadirfan <amadirfan443@gmail.com>
2024-02-26 22:29:30 +0500

    update with readme

---
+++ README.md
@@ -0,0 +1,40 @@
+#Web Application using HTML & CSS
+
+---------------------
+
+|    Submitted By    |        AmadIrfan(2021-CS-25)         |
+| :----------------: | :----------------------------------: |
+| **Project Name  ** | **Web Application with HTML & CSS3** |
+
+###***Description:***
+
+This web app representing the service of a restaurant offering  multiple services for their customers. This app contains a home page ,about page for info related to restaurant a menu page for offering menus and services page which services are providing to the customers. 
+
+#### How to run project :
+
+1. **Clone Project Repository : **
+
+   Clone a project repository from GITHUB to your local system.
+
+2. **Start a Local Server : **
+
+   open root folder of web app in vs code and press `f5`or `f10` to run project locally. 
+
+   ####Summary on findings :
+
+   I make a simple restaurant app with HTML & CSS3 learning this app contains transition Grid layout flex box and many other concepts.
+
+   #### Challenges faced:
+
+   I face more challenge in make app responsive using` MEDIA QURIES`
+
+   #### Resources Link:
+
+   Recourses use in app:
+
+   [w3School](Https://www.w3schools.com/)
+
+   [link 2](https://developer.mozilla.org)
+
+   ​
+
--- js/script.js
+++ js/script.js
@@ -1,41 +1,19 @@
 // @ts-nocheck
-// document.addEventListener("DOMContentLoaded", () => {
-// });
+document.addEventListener("DOMContentLoaded", () => {
+  let nav_menu = document.getElementsByClassName("nav-menu")[0];
+  let btnMenuShow = document.getElementById("responsive-btn");
+  let contactBtn = document.getElementById("contact-btn");
 
-let slideIndex = 0;
-let nav_menu = document.getElementsByClassName("nav-menu")[0];
-let btnMenuShow = document.getElementById("responsive-btn");
-const slides = document.getElementsByClassName("slide");
+  btnMenuShow?.addEventListener("click", () => {
+    nav_menu.classList.toggle("show-nav-menu");
+  });
 
-btnMenuShow?.addEventListener("click", () => {
-  nav_menu.classList.toggle("show-nav-menu");
+  nav_menu.addEventListener("click", () => {
+    nav_menu.classList.toggle("show-nav-menu");
+  });
+
+  contactBtn.addEventListener("click", (e) => {
+    console.log('this');
+    window.location.pathname = '../pages/contact.html';
+  });
 });
-
-nav_menu.addEventListener("click", () => {
-  nav_menu.classList.toggle("show-nav-menu");
-});
-
-function showSlide(n) {
-  for (let i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  slides[n].style.display = "block";
-}
-
-function nextSlide() {
-  slideIndex++;
-  if (slideIndex >= slides.length) {
-    slideIndex = 0;
-  }
-  showSlide(slideIndex);
-}
-
-function prevSlide() {
-  slideIndex--;
-  if (slideIndex < 0) {
-    slideIndex = slides.length - 1;
-  }
-  showSlide(slideIndex);
-}
-
-showSlide(slideIndex);
